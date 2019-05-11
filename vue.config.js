8baa28649bd7530d59cce3f61ee8624ab3409c33
smitpatelx <smitbpatel1999@gmail.com>
2019-05-10 22:43:50 -0400

    Makink deployment easy with Netlify

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@
 // make sure you update `yourProjectName` with the name of your GitLab project
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/vuetify_todo/' : '/'
+  publicPath: process.env.BASE_URL === 'production' ? '/vuetify_todo/' : '/'
 };
